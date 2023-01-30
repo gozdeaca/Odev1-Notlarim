08de2a328fd7a97404c48001103daf1b22e13914
gozdeaca <gozdeaca5@gmail.com>
2023-01-30 14:44:58 +0300

    Google OUA Görevi

---
+++ main.cs
@@ -0,0 +1,14 @@
+
+using System;
+class Program {
+  static void Main(string[] args ) {
+      int ilksayi = 35;
+      
+      int ikincisayi = ilksayi + 5;
+      ikincisayi += 10;
+      
+    Console.WriteLine(ikincisayi);
+    Console.ReadLine();
+    
+  }
+}
